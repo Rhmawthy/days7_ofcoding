0c3c892e7360bd19924056434fa57d6ad18cda5f
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-10-17 00:44:35 -0700

    Update day7_latihan penggunaan operator aritmatika.py

--- day7_latihan penggunaan operator aritmatika.py
+++ day7_latihan penggunaan operator aritmatika.py
@@ -6,9 +6,9 @@ tinggi        = int(input("masukkan tinggi prisma:"))
 keliling_alas = int(input("masukkan keliling alas prisma:"))
 
 volume        = luas_alas * tinggi
-Lp_prisma   = (2 * luas_alas) + ( keliling_alas * tinggi )
+lp_prisma   = (2 * luas_alas) + ( keliling_alas * tinggi )
 
-print ("volume prisma",volume, "Cm3  dan luas prisma", Lp_prisma,"cm3")
+print ("volume prisma",volume, "Cm3  dan luas prisma", lp_prisma,"cm3")
 
 
  
