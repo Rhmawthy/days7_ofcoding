37b28627de896f023c3a79cd05c039eddb1acb11
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-10-17 00:36:43 -0700

    Add files via upload

---
+++ day7_latihan penggunaan operator aritmatika.py
@@ -0,0 +1,15 @@
+# meghitung volume dan luas permukaan
+
+#menghitung volume prisma segitiga
+
+luas_alas     = int(input( "masukkan luas alas prisma:"))
+tinggi        = int(input("masukkan tinggi prisma:"))
+keliling_alas = int(input("masukkan keliling alas prisma:"))
+
+volume        = luas_alas * tinggi
+Lp_prisma   = (2 * luas_alas) + ( keliling_alas * tinggi )
+
+print ("volume prisma",volume, "Cm3  dan luas prisma", Lp_prisma,"cm3")
+
+
+ 
